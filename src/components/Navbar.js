92205fb3ccd40607706ae9755ea7a06df64d41d5
Shubham Nikam <114049680+shubhamsnikam@users.noreply.github.com>
2025-05-15 12:18:40 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,40 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../App.css';
 
 const Navbar = () => (
-  <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4">
-    <Link className="navbar-brand" to="/"> अलंकृत </Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+  <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top px-4 py-3">
+    <NavLink className="navbar-brand" to="/"> अलंकृत </NavLink>
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav ms-auto">
-        <li className="nav-item">
-          <Link className="nav-link" to="/products">Stock</Link>
+        <li className="nav-item px-2">
+          <NavLink className="nav-link" to="/products">Stock</NavLink>
         </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/sales">Sales</Link>
+        <li className="nav-item px-2">
+          <NavLink className="nav-link" to="/sales">Sales</NavLink>
         </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/customers">Customers</Link>
+        <li className="nav-item px-2">
+          <NavLink className="nav-link" to="/customers">Customers</NavLink>
         </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/reports">Reports</Link>
+        <li className="nav-item px-2">
+          <NavLink className="nav-link" to="/reports">Reports</NavLink>
         </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/ledger">Ledger</Link>
+        <li className="nav-item px-2">
+          <NavLink className="nav-link" to="/ledger">Ledger</NavLink>
         </li>
-   <li className="nav-item px-2">
+        <li className="nav-item px-2">
           <NavLink className="nav-link" to="/help">Help</NavLink>
         </li>
       </ul>
